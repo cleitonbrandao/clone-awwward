7ca807defd1684b0249608e05cf33dd0aa4e1783
cleitonbrandao <cbs3dmax@gmail.com>
2025-03-18 18:04:24 -0300

    animation main and section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <main className="relative flex items-center justify-center h-screen bg-background">
+    <>
+    <main className="animate-scroll-up-out relative flex items-center justify-center h-screen bg-background">
       <video
         className="absolute top-0 left-0 w-full h-full object-cover"
         autoPlay
@@ -37,18 +38,34 @@ export default function Home() {
             protein + caffeine
           </h1>
         </div>
-        <div className="w-lg animate-delay-1000 animate-fade-up animate-duration-500 mb-20">
+        <div className="w-lg animate-delay-1000 animate-fade-up animate-duration-500 mb-[clamp(1.6rem, 5vw, 4rem)]">
           <p className="text-center overflow-auto whitespace-pre-line normal-case text-dark-brown font-montserrat text-lg p-5">
             Live life to the fullest with SPYLT: Shatter boredom and embrace
             your inner kid every deliciously smooth chug.
           </p>
         </div>
-        <div className="bg-light-brown p-4 rounded-full">
-          <a className="upercase font-antonio font-bold p-8 py-4 text-dark-brown">
-            ghug a spylt
+        <div className="animate-delay-1000 animate-fade-up duration-500 bg-light-brown p-4 rounded-full">
+          <a
+            className="uppercase font-antonio font-bold px-8 py-4 text-dark-brown cursor-pointer"
+            href="#"
+          >
+            Chug a Spylt
           </a>
         </div>
       </div>
     </main>
+    <section className="animation-scroll-reveal h-screen z-10 pt-[7vw] bg-background-section">
+      <div className="flex flex-col items-center h-full uppercase font-bold p-0 m-0 -tracking-[.25rem]">
+        <h1 className="text-8xl"><span>stir up your</span></h1>
+        <h1 className="text-8xl"><span>fearless past and</span></h1>
+          <div className="animation-scroll-reveal-fade-in relative z-10 rotate-3 bg-light-brown border-10 m-0 pt-4 border-background-section flex items-center justify-center animate-reveal-horizontal">
+            <div className="-mt-2 tracking-tight text-8xl text-background-section uppercase text-center px-2 pb-5 origin-center">fuel up</div>
+          </div>
+        <div className="-mt-12 text-8xl text-justify"><h1><span>your future with every</span></h1><h1><span>glup of perfect protein</span></h1></div>
+      </div>
+    </section>
+    <div className="animation-scroll-reveal h-screen z-10 pt-[7vw] bg-dark-brown">
+</div>
+    </>
   );
 }
